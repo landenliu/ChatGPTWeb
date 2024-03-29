291644969f200539bf75b6d65269460c49ca9a79
landenliu <131243030+landenliu@users.noreply.github.com>
2024-03-29 11:31:32 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "SY Chat",
+  description: "personal Only.",
   viewport: {
     width: "device-width",
     initialScale: 1,
